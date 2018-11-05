5fa6a66f234307fd3725802139a8b34e66afb854
Shantanu <shantanu.067@gmail.com>
2018-11-05 00:15:16 -0700

    adding player controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_playerTransform = null;
+
+    [SerializeField]
+    private float m_smoothFactor = 0.5f;
+
+    private bool bLookAtPlayer = false;
+    private Vector3 m_cameraOffset;
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+        m_cameraOffset = transform.position - m_playerTransform.position;		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector3 newPosition = m_playerTransform.position + m_cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPosition, m_smoothFactor);
+
+        if (bLookAtPlayer)
+            transform.LookAt(m_playerTransform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speed = 10f;
+
+    private Vector3 m_moveDir = Vector3.zero;
+
+    private CharacterController m_controller;
+	// Use this for initialization
+	void Start ()
+    {
+        m_controller = this.GetComponent<CharacterController>();		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_controller.isGrounded)
+        {
+            m_moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            m_moveDir = transform.TransformDirection(m_moveDir);
+            m_moveDir *= m_speed;
+            m_controller.Move(m_moveDir * Time.deltaTime);
+        }
+	}
+}
